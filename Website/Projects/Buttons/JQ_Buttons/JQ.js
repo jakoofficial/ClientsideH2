d7b77e6c3058e1a94a8281a7949acb328ba8cf35
jakoofficial <jacobvoandersen@gmail.com>
2023-08-24 17:34:14 +0200

    Issue with getting specific button

--- Website/Projects/Buttons/JQ_Buttons/JQ.js
+++ Website/Projects/Buttons/JQ_Buttons/JQ.js
@@ -1,28 +1,41 @@
 var ButtonAddedCounter = 0;
+var SelectedButton;
 var CurrentButton;
+var getDiv;
 
 var txtButtonContext = $("#txtButtonContext");
 var ButtonAddedClass = $(".ButtonsAdded");
+var btnDelete = $("#btnDeleteDiv");
 
 $("#btnAddNewButton").click(function () {
     ++ButtonAddedCounter; //Adds 1 to The counter
-    var divNew = $("<div></div>").attr("ID", "btnDiv"+ ButtonAddedCounter);
+    divNew = $("<div></div>").attr("ID", "btnDiv"+ ButtonAddedCounter);
     var btn = createButton();
+
     ButtonAddedClass.append(divNew);
-    ButtonAddedClass.append(createButton());
+    ButtonAddedClass.append(btn);
     
-    var getDiv = $("#btnDiv"+ ButtonAddedCounter);
+    getDiv = $("#btnDiv"+ ButtonAddedCounter);
     var getBtn = $("#btnDynamic"+ ButtonAddedCounter);
 
-
     getDiv.append(getBtn);
-    console.log(ButtonAddedClass);
+    // console.log(ButtonAddedClass);
 });
+
+$(".ButtonsAdded input").on("click", function(){
+    SelectBtn($(this));
+});
+
+function SelectBtn(btn) {
+    // CurrentButton = btn;
+    console.log(btn);
+}
 
 function createButton() {
     var btnString = "<input id='btnDynamic" + ButtonAddedCounter + "' ";
     btnString += "value='btnDynamic" + ButtonAddedCounter + "' ";
     btnString += "type='button'";
+    // btnString += "click='"+ SelectBtn(this) +"'";
     btnString += " />";
     return btnString;
 }
@@ -32,4 +45,11 @@ function CreateFunctionBtn(text, clickfunc){
     btnString += "type='button' onclick='"+ clickfunc +"'";
     btnString += "/>";
     return btnString;
+}
+
+function DeleteDiv(divID){
+    var delBtnDiv = divID;
+    // delBtnDiv.remove();
+    console.log(divID);
+    // $("#"+ div.attr("ID") +"").remove();
 }
