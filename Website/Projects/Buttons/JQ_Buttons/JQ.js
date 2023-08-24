ccf68705d51ef98bd10fc728f50e3928ca2cc82e
jakoofficial <jacobvoandersen@gmail.com>
2023-08-24 16:41:18 +0200

    Made it so buttons get created through Jquery and added to the new div created beforehand.

--- Website/Projects/Buttons/JQ_Buttons/JQ.js
+++ Website/Projects/Buttons/JQ_Buttons/JQ.js
@@ -5,19 +5,25 @@ var txtButtonContext = $("#txtButtonContext");
 var ButtonAddedClass = $(".ButtonsAdded");
 
 $("#btnAddNewButton").click(function () {
-    var div = "<div id='btn"+ButtonAddedCounter.toString()+"'></div>";
+    ++ButtonAddedCounter; //Adds 1 to The counter
+    var divNew = $("<div></div>").attr("ID", "btnDiv"+ ButtonAddedCounter);
     var btn = createButton();
-    // var btnFunc = CreateFunctionBtn();
-    div.append(btn);
-    ButtonAddedClass.append(div);
+    ButtonAddedClass.append(divNew);
+    ButtonAddedClass.append(createButton());
+    
+    var getDiv = $("#btnDiv"+ ButtonAddedCounter);
+    var getBtn = $("#btnDynamic"+ ButtonAddedCounter);
+
+
+    getDiv.append(getBtn);
+    console.log(ButtonAddedClass);
 });
 
 function createButton() {
-    var btnString = "<input id='btnDynamic" + ++ButtonAddedCounter + "' ";
+    var btnString = "<input id='btnDynamic" + ButtonAddedCounter + "' ";
     btnString += "value='btnDynamic" + ButtonAddedCounter + "' ";
     btnString += "type='button'";
     btnString += " />";
-    btnString += "<br>";
     return btnString;
 }
 
