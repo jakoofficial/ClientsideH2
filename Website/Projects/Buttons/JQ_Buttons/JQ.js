5401dd8ce62e1b017b1d5f9d70d8da7f981f8313
Jacob Ørnskov Andersen <jacobvoandersen@gmail.com>
2023-08-25 10:19:49 +0200

    Fixed issues with buttons and now have the ability for deletion and editing of buttons. Got Help from Haraldür.

--- Website/Projects/Buttons/JQ_Buttons/JQ.js
+++ Website/Projects/Buttons/JQ_Buttons/JQ.js
@@ -1,55 +1,116 @@
-var ButtonAddedCounter = 0;
-var SelectedButton;
-var CurrentButton;
-var getDiv;
+$(document).ready(function(){
+    var ButtonAddedCounter = 0;
+    var textBoxElement;
+    var documentButton;
 
-var txtButtonContext = $("#txtButtonContext");
-var ButtonAddedClass = $(".ButtonsAdded");
-var btnDelete = $("#btnDeleteDiv");
-
-$("#btnAddNewButton").click(function () {
-    ++ButtonAddedCounter; //Adds 1 to The counter
-    divNew = $("<div></div>").attr("ID", "btnDiv"+ ButtonAddedCounter);
-    var btn = createButton();
-
-    ButtonAddedClass.append(divNew);
-    ButtonAddedClass.append(btn);
+    // var txtButtonContext = $("#txtButtonContext");
+    // var ButtonAddedClass = $(".ButtonsAdded");
+    // var btnDelete = $("#btnDeleteDiv");
     
-    getDiv = $("#btnDiv"+ ButtonAddedCounter);
-    var getBtn = $("#btnDynamic"+ ButtonAddedCounter);
+    SetupDOMElements();
 
-    getDiv.append(getBtn);
-    // console.log(ButtonAddedClass);
+    function SetupDOMElements() {
+        textBoxElement = $("#txtButtonContextJq");
+    
+        $("#btnAddNewButton").click(function () {
+          createButton();
+        });
+    
+        textBoxElement.on("keyup", function (event) {
+          if (event.key === "Enter") {
+            TextboxValueChanged();
+          }
+        });
+    
+        $("#txtButtonContextJq").on("blur", function () {
+          TextboxValueChanged();
+        });
+      }
+
+    $(".ButtonsAdded input").on("click", function(){
+        SelectBtn($(this));
+    });
+
+    function createButton() {
+        if (ButtonAddedCounter <= 9) {
+
+            var d = $("<div>");
+            $(".ButtonsAdded").append(d);
+            
+            var names = ["Button", "Delete", "Edit"];
+            for(var i = 0; i<names.length; i++){
+                setUpButton(`Name:JQ - ${names[i]}. Id:${i}. Row:${ButtonAddedCounter}.`)
+            }
+            ButtonAddedCounter++; //Adds 1 to The counter
+        }
+    }
+    
+    function setUpButton(name){
+        var nameSplit = name.split(".")[0].split(":")[1];
+        var textValue = "Button_"+ButtonAddedCounter;
+        
+        if (nameSplit == "JQ - Button") {
+            var button = $("<input>", {
+                type: "button",
+                id: name,
+                value: textValue
+            });    
+        }
+        else {
+            var button = $("<input>", {
+                type: "button",
+                id: name,
+                value: nameSplit
+            });
+        }
+
+        button.click(function(){
+            btnPressed(name);
+        });
+    
+        $(".ButtonsAdded").append(button);
+    }
+
+    function UpdateButtonValue(){
+        
+    }
+
+    function btnPressed(name){
+        var id = name.split(".")[1].split(":")[1];
+        var row = name.split(".")[2].split(":")[1];
+        
+        if (id == 1) {
+            DeleteDiv(row);
+        } else if (id == 2) {
+            editButtonClicked(row);
+        }
+
+    }
+
+    function DeleteDiv(row){
+        textBoxElement.prop("disabled", true).val("").hide();
+
+        $('[id*="Name:JQ"][id*="Row:' + row + '."]').each(function () {
+        $(this).remove();
+        });
+        ButtonAddedCounter--;
+        console.log(ButtonAddedCounter);
+    }
+
+    function editButtonClicked(row) {
+        var getButton = `Name:JQ - Button. Id:0. Row:${row}.`;
+        var escapedId = getButton.replace(/(:|\.|\s)/g, "\\$1");
+        documentButton = $(`#${escapedId}`);
+        textBoxElement
+          .prop("disabled", false)
+          .val(documentButton.val())
+          .show()
+          .focus();
+      }
+
+      function TextboxValueChanged() {
+        documentButton.val(textBoxElement.val());
+        textBoxElement.prop("disabled", true).val("").hide();
+      }
+
 });
-
-$(".ButtonsAdded input").on("click", function(){
-    SelectBtn($(this));
-});
-
-function SelectBtn(btn) {
-    // CurrentButton = btn;
-    console.log(btn);
-}
-
-function createButton() {
-    var btnString = "<input id='btnDynamic" + ButtonAddedCounter + "' ";
-    btnString += "value='btnDynamic" + ButtonAddedCounter + "' ";
-    btnString += "type='button'";
-    // btnString += "click='"+ SelectBtn(btnString.attr("id")) +"'";
-    btnString += " />";
-    return btnString;
-}
-
-function CreateFunctionBtn(text, clickfunc){
-    var btnString = "<input value='"+text+"'";
-    btnString += "type='button' onclick='"+ clickfunc +"'";
-    btnString += "/>";
-    return btnString;
-}
-
-function DeleteDiv(divID){
-    var delBtnDiv = divID;
-    // delBtnDiv.remove();
-    console.log(divID);
-    // $("#"+ div.attr("ID") +"").remove();
-}
