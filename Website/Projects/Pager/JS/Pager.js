ee0534a153f4d7a34aafd6a804165e7ce8c44f89
jakoofficial <jacobvoandersen@gmail.com>
2023-08-31 20:22:37 +0200

    Added page functionality with button creation under current page. Done in JS, Might need JQ version

--- Website/Projects/Pager/JS/Pager.js
+++ Website/Projects/Pager/JS/Pager.js
@@ -1,19 +1,76 @@
 
-$(document).ready(function(){
-    $(".btn").on('click', function(){
-        CreateButton("Hellow");
-        
-    });
-    
-    // CreateButton("Testing");
-    
-    function CreateButton(btnValue){
-        var button = $("<input>", {
-            type: "button",
-            value: btnValue,
-            class: "btnShow"
-        });
-        $("#buttonArea").append(button);
-    }
+var pageCounter = 0;
+var currentPage;
 
-});
+//Adds a new page and automaticly sets it as current page
+//This for easy first page when load
+function CreatePage() {
+    ++pageCounter;
+    var button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('ID', "p" + pageCounter.toString());
+    button.setAttribute('value', "page" + pageCounter.toString());
+    button.setAttribute('onclick', "SetPageAsCurrent("+ button.id +")");
+    document.getElementById("pageArea").append(button);
+
+    SetPageAsCurrent(button);
+}
+
+//Creates the button for the current page active
+function CreateButton() {
+    var button = document.createElement('input');
+    button.setAttribute('type', 'button');
+    button.setAttribute('ID', currentPage);
+    button.setAttribute('value', currentPage);
+    document.getElementById("buttonArea").append(button);
+}
+
+//Sets the current page
+function SetPageAsCurrent(b) {
+    var page = b;
+    currentPage = page.id; //Sets page as current
+
+    var divPages = document.getElementById('pageArea'); //Gets area of pages
+    //Goes through all page buttons and removes CurrentPage class
+    for(let i = 0; i < divPages.children.length; i++) {
+        divPages.children[i].classList.remove('CurrentPage');
+    }
+    
+    var divPageBtns = document.getElementById('buttonArea'); //Gets area of page buttons
+    //Goes through all buttons on pages
+    for(let i = 0; i < divPageBtns.children.length; i++) {
+        var curBtn = divPageBtns.children[i]; //Sets curbtn to button on page
+        //If curBtn is not part of CurrentPage make invisible
+        //Else make visible
+        if (curBtn.id != currentPage) {
+            curBtn.classList.add('BtnNotVisible');
+        }
+        else {
+            curBtn.classList.remove('BtnNotVisible');
+        }
+    }
+    //Add page button as Current
+    page.classList.add('CurrentPage');
+}
+
+// $(document).ready(function(){
+
+//     var pageCounter = 0;
+
+//     $(".btn").on('click', function(){
+//         CreateButton("Hellow");
+//     });
+    
+//     // CreateButton("Testing");
+    
+//     function CreateButton(btnValue){
+//         var button = $("<input>", {
+//             type: "button",
+//             value: btnValue,
+//             class: "btnShow",
+//             name: "page"+ ++pageCounter
+//         });
+//         $("#buttonArea").append(button);
+//     }
+
+// });
