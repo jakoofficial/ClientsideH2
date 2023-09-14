5e50ecd27bed0d8c0cb536c6549b9caddd9d4f04
jakoofficial <jacobvoandersen@gmail.com>
2023-09-14 11:34:07 +0200

    Fixed issues with --watch. Made the calculator working with interface and classes

--- Website/Projects/TypeScriptTesting/out/SetUp.js
+++ Website/Projects/TypeScriptTesting/out/SetUp.js
@@ -1,17 +1,111 @@
 "use strict";
-var Player = /** @class */ (function () {
-    function Player(balance) {
-        this.balance = balance;
+var newButton = /** @class */ (function () {
+    function newButton(text) {
+        this.text = text;
     }
-    return Player;
-}());
-var Item = /** @class */ (function () {
-    function Item(itemName, sellInPrice) {
-        this.itemName = itemName;
-        this.sellPrice = sellInPrice;
-    }
-    Item.prototype.sell = function (p) {
-        p.balance += this.sellPrice;
+    newButton.prototype.action = function () {
+        var _this = this;
+        return function () {
+            calculation.push(_this.text);
+            updateText();
+            console.log(_this.text);
+        };
     };
-    return Item;
+    return newButton;
 }());
+var newFuncButton = /** @class */ (function () {
+    function newFuncButton(text, operator) {
+        this.text = text;
+        this.operator = operator;
+    }
+    newFuncButton.prototype.action = function () {
+        var _this = this;
+        return function () {
+            if (_this.text == '=') {
+                calculate(_this.text);
+            }
+            else if (_this.text == 'C') {
+                clearCalc();
+                updateText();
+            }
+            else {
+                calculation.push(_this.text);
+                updateText();
+            }
+        };
+    };
+    return newFuncButton;
+}());
+var calculation = [];
+var pa = document.getElementById('calculationArea');
+function updateText() {
+    var temp = "";
+    for (var i = 0; i < calculation.length; i++) {
+        temp += calculation[i].toString();
+    }
+    if (pa == null)
+        return;
+    pa.innerHTML = temp;
+    console.log(temp);
+}
+function clearCalc() {
+    if (pa == null)
+        return;
+    pa.innerHTML = "";
+    calculation = [];
+}
+function getCalcOperator() {
+    if (pa == null)
+        return;
+    for (var i = 0; i < pa.innerHTML.length; i++) {
+        for (var j = 0; j < calcOpe.length; j++) {
+            if (calcOpe[j] == pa.innerHTML[i]) {
+                return calcOpe[j].toString();
+            }
+        }
+    }
+    return "NaN";
+}
+function calculate(ope) {
+    var splitCalc = getCalcOperator();
+    if (splitCalc == null || pa == null)
+        return;
+    var paSplit = pa.innerHTML.split(splitCalc);
+    var calcRes = eval(paSplit[0] + splitCalc + paSplit[1]);
+    pa.innerHTML = calcRes;
+    console.log(pa.innerHTML);
+    // console.log(pa.innerHTML.split(splitCalc));
+}
+function createButton(btn) {
+    var n = btn.action();
+    var b = document.createElement("input");
+    b.type = "button";
+    b.value = btn.text;
+    b.onclick = btn.action();
+    return b;
+}
+function createFuncButton(btn) {
+    var n = btn.action();
+    var b = document.createElement("input");
+    b.type = "button";
+    b.value = btn.text;
+    b.onclick = btn.action();
+    return b;
+}
+var calcOpe = ['C', '+', '-', '*', '/', '='];
+function setUpButtons() {
+    var _a, _b;
+    for (var i = 0; i <= 9; i++) {
+        var nb = new newButton((i).toString());
+        (_a = document.getElementById('calcBtnArea')) === null || _a === void 0 ? void 0 : _a.append(createButton(nb));
+    }
+    for (var j = 0; j < calcOpe.length; j++) {
+        var nfb = new newFuncButton(calcOpe[j], calcOpe[j]);
+        var te = (_b = document.getElementById('functionBtnArea')) === null || _b === void 0 ? void 0 : _b.append(createFuncButton(nfb));
+    }
+}
+setUpButtons();
+// let nb1 = new newButton("1");
+// let nb2 = new newButton("2");
+// document.getElementById('test')?.append(createButton(nb1));
+// document.getElementById('test')?.append(createButton(nb2));
--- Website/Projects/TypeScriptTesting/src/SetUp.ts
+++ Website/Projects/TypeScriptTesting/src/SetUp.ts
@@ -1,19 +1,121 @@
-class Player{
-    balance: number;
-    constructor(balance:number) {
-        this.balance = balance;
+interface IcalcBtn {
+    text:string;
+    action(): () => void;
+}
+
+class newButton implements IcalcBtn {
+    text: string;
+
+    constructor(text:string){
+        this.text = text;
+    }
+
+    action() {
+        return () => {
+            calculation.push(this.text);
+            updateText();
+            console.log(this.text);
+        }
     }
 }
 
-class Item {
-    itemName: string; 
-    sellPrice: number;
-    constructor(itemName: string, sellInPrice: number) {
-        this.itemName = itemName;
-        this.sellPrice= sellInPrice;
+class newFuncButton implements IcalcBtn {
+    text: string;
+    operator:string;
+
+    constructor(text:string, operator:string){
+        this.text = text;
+        this.operator = operator;
     }
 
-    sell(p:Player) {
-        p.balance += this.sellPrice;
+    action() {
+        return () => {
+            if (this.text == '=') {calculate(this.text);}
+            else if(this.text == 'C'){clearCalc(); updateText();}
+            else 
+            {
+                calculation.push(this.text);
+                updateText();
+            }
+        };
     }
 }
+
+let calculation : string[] = [];
+
+let pa = document.getElementById('calculationArea');
+function updateText(){
+    var temp = "";
+    for (let i = 0; i < calculation.length; i++) {
+        temp += calculation[i].toString()
+    }
+    if (pa == null) return;
+    
+    pa.innerHTML = temp;
+    console.log(temp);
+}
+function clearCalc(){
+    if (pa == null) return;
+    pa.innerHTML = "";
+    calculation = [];
+}
+
+function getCalcOperator(){
+    if (pa == null) return;
+    for (let i = 0; i < pa.innerHTML.length; i++){
+        for (let j = 0; j<calcOpe.length; j++){
+            if (calcOpe[j] == pa.innerHTML[i]){
+                return calcOpe[j].toString();
+            }
+        }
+    }
+    return "NaN";
+}
+
+function calculate(ope:string){
+    let splitCalc = getCalcOperator();
+    if (splitCalc == null || pa == null) return;
+    const paSplit = pa.innerHTML.split(splitCalc);
+    let calcRes = eval(paSplit[0] + splitCalc + paSplit[1])
+
+    pa.innerHTML = calcRes;
+    console.log(pa.innerHTML);
+    // console.log(pa.innerHTML.split(splitCalc));
+
+}
+
+function createButton(btn : newButton){
+    const n = btn.action();
+    var b = document.createElement("input");
+    b.type="button";
+    b.value = btn.text;
+    b.onclick = btn.action();
+    return b;
+}
+function createFuncButton(btn : newFuncButton){
+    const n = btn.action();
+    var b = document.createElement("input");
+    b.type="button";
+    b.value = btn.text;
+    b.onclick = btn.action();
+    return b;
+}
+
+const calcOpe = ['C', '+', '-', '*', '/', '=']
+
+function setUpButtons(){
+    for (let i=0; i<=9; i++){
+        let nb = new newButton((i).toString());
+        document.getElementById('calcBtnArea')?.append(createButton(nb));
+    }
+    for (let j=0; j<calcOpe.length; j++) {
+        let nfb = new newFuncButton(calcOpe[j], calcOpe[j]);
+        var te = document.getElementById('functionBtnArea')?.append(createFuncButton(nfb));
+    }
+}
+setUpButtons();
+
+// let nb1 = new newButton("1");
+// let nb2 = new newButton("2");
+// document.getElementById('test')?.append(createButton(nb1));
+// document.getElementById('test')?.append(createButton(nb2));
